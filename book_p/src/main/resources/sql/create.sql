e9ebd90a71d2536348d4c132285a0e19285ebe4f
ejImDev <ejim0093@gmail.com>
2024-01-16 11:49:19 +0900

    240116 - SQL fix) 메일내역테이블, 이메일인증테이블 생성

--- book_p/src/main/resources/sql/create.sql
+++ book_p/src/main/resources/sql/create.sql
@@ -14,7 +14,6 @@ CREATE TABLE `P_USER` (
     `user_name` varchar(100) NOT NULL COMMENT '이름',
     `user_phone` varchar(20) NOT NULL COMMENT '전화번호',
     `user_type` tinyint(4) DEFAULT 0 COMMENT '회원 권한 (0:임시회원, 1:정회원, 2:탈퇴회원, 99:관리자)',
-    `confirm_code` varchar(100) DEFAULT NULL COMMENT '회원가입 인증 코드',
     `create_dt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '가입일시',
     `update_dt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '업데이트 일시',
     PRIMARY KEY (`idx_user`) USING BTREE
@@ -25,7 +24,29 @@ CREATE TABLE `P_USER_HISTORY` (
     `idx_login` int(11) NOT NULL AUTO_INCREMENT COMMENT '인덱스',
     `idx_user` int(11) NOT NULL COMMENT '회원 인덱스',
     `user_type` tinyint(4) NOT NULL COMMENT '회원 권한 (0:임시회원, 1:정회원, 2:탈퇴회원, 99:관리자)',
-    `remoteIP` datetime NOT NULL COMMENT '접속 정보',
+    `remoteIP` varchar(50) NOT NULL COMMENT '접속 정보',
     `create_dt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '로그인 일시',
     PRIMARY KEY (`idx_login`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='로그인 정보';
+
+DROP TABLE P_MAIL_HISTORY;
+CREATE TABLE `P_MAIL_HISTORY` (
+    `idx_mail` int(11) NOT NULL AUTO_INCREMENT COMMENT '인덱스',
+    `receiver` varchar(255) NOT NULL COMMENT '수신 이메일',
+    `mail_type` tinyint(4) NOT NULL COMMENT '메일 타입 (0:인증메일, 1:임시비밀번호 발급...)',
+    `mail_title` varchar(255) NOT NULL COMMENT '메일 내용',
+    `mail_content` TEXT NOT NULL COMMENT '메일 내용'
+    `idx_user` int(11) COMMENT '회원 인덱스',
+    `create_dt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '메일 발송 일시',
+    PRIMARY KEY (`idx_mail`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='메일 정보';
+
+DROP TABLE P_USER_AUTH;
+CREATE TABLE `P_USER_AUTH` (
+    `idx_auth` int(11) NOT NULL AUTO_INCREMENT COMMENT '인덱스',
+    `receiver` varchar(255) NOT NULL COMMENT '수신 이메일',
+    `authKey` int(6) NOT NULL COMMENT '인증키',
+    `auth_type` tinyint(1) NOT NULL DEFAULT 0 COMMENT '인증 타입 (0:인증키발급, 1:인증완료...)',
+    `create_dt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '메일 발송 일시',
+    PRIMARY KEY (`idx_auth`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='회원 인증 정보';
